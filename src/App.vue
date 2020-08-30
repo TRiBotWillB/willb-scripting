4ccb5e15e7b5b8172143fbc4d655933e810f4332
Will Baker <willbaker698@gmail.com>
2020-08-30 21:44:37 +0100

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+
+        <nav>
+            <ul class="list-inline">
+                <li class="list-inline-item">
+                    Home
+                </li>
+                <li class="list-inline-item">
+                    Scripts
+                </li>
+                <li class="list-inline-item">
+                    Contact
+                </li>
+            </ul>
+        </nav>
+
+        <b-container>
+            <b-row>
+                <Script scriptName="FARMER" price="$9.99" imgSrc="img/Farmer.png"/>
+            </b-row>
+        </b-container>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import Script from './components/Script.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            Script
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ src/components/Script.vue
@@ -0,0 +1,48 @@
+<template>
+    <b-col cols="3">
+        <div class="thumbnail">
+            <img :src="imgSrc">
+            <div class="caption-left">{{price}}</div>
+        </div>
+
+        <h3 class="script-title">{{scriptName}}</h3>
+    </b-col>
+</template>
+
+<script>
+
+    export default {
+        name: 'Script',
+        props: {
+            scriptName: String,
+            price: String,
+            imgSrc: String,
+        }
+
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .thumbnail img {
+        max-width: 100%;
+    }
+
+    h3 {
+        margin: 40px 0 0;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
+</style>
